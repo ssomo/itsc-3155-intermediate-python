69e8e002615801cbfb469c8c749b9bbe5de9e373
ssomo <sofia.mohamed.733@gmail.com>
2023-09-03 20:47:58 -0400

    Added comments to the script

--- script_4.py
+++ script_4.py
@@ -1,12 +1,21 @@
+#To hold the sum
 sum = 0
 
+#Loops 5 times
 for i in range(5):
-    idx = i + 1 
+    idx = i + 1 #index number
+    #Prompts the user to enter an int
     num = (input('Enter int #{}: '.format(idx)))
+    #Checks if the value inputted is not an int
     while not num.isdigit():
+        #Error Message
         print('Invalid input. Please enter an int.')
+        #Prompts the user to enter the int again
         num = input('Enter int #{}: '.format(idx))
+    #Formats the input into an int
     num = int(num)
+    #Sums the numbers
     sum += num
 
+#Prints out the sum
 print('Your sum is', sum)
